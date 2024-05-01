a66e2ab57c77cb4600859cfc836e213456fa30f1
Reuben Frankel <rfrankel@matatika.com>
2024-05-01 01:54:04 +0100

    Refactor away `CustomObject`

--- tap_trello/schemas/actions.py
+++ tap_trello/schemas/actions.py
@@ -2,93 +2,89 @@
 
 from singer_sdk import typing as th
 
-from tap_trello.schemas.utils.custom_object import CustomObject
-
-
-class ActionsObject(CustomObject):
-    properties = th.PropertiesList(
-        th.Property("id", th.StringType),
-        th.Property("idMemberCreator", th.StringType),
-        th.Property(
-            "data",
-            th.ObjectType(
-                th.Property(
-                    "card",
-                    th.ObjectType(
-                        th.Property("id", th.StringType),
-                        th.Property("name", th.StringType),
-                        th.Property("desc", th.StringType),
-                        th.Property("idList", th.StringType),
-                        th.Property("idShort", th.NumberType),
-                        th.Property("shortLink", th.StringType),
-                    ),
+ActionsObject = th.PropertiesList(
+    th.Property("id", th.StringType),
+    th.Property("idMemberCreator", th.StringType),
+    th.Property(
+        "data",
+        th.ObjectType(
+            th.Property(
+                "card",
+                th.ObjectType(
+                    th.Property("id", th.StringType),
+                    th.Property("name", th.StringType),
+                    th.Property("desc", th.StringType),
+                    th.Property("idList", th.StringType),
+                    th.Property("idShort", th.NumberType),
+                    th.Property("shortLink", th.StringType),
                 ),
-                th.Property(
-                    "old",
-                    th.ObjectType(
-                        th.Property("name", th.StringType),
-                        th.Property("desc", th.StringType),
-                        th.Property("idList", th.StringType),
-                    ),
+            ),
+            th.Property(
+                "old",
+                th.ObjectType(
+                    th.Property("name", th.StringType),
+                    th.Property("desc", th.StringType),
+                    th.Property("idList", th.StringType),
                 ),
-                th.Property(
-                    "board",
-                    th.ObjectType(
-                        th.Property("id", th.StringType),
-                        th.Property("name", th.StringType),
-                        th.Property("shortLink", th.StringType),
-                    ),
+            ),
+            th.Property(
+                "board",
+                th.ObjectType(
+                    th.Property("id", th.StringType),
+                    th.Property("name", th.StringType),
+                    th.Property("shortLink", th.StringType),
                 ),
-                th.Property(
-                    "list",
-                    th.ObjectType(
-                        th.Property("id", th.StringType),
-                        th.Property("name", th.StringType),
-                    ),
+            ),
+            th.Property(
+                "list",
+                th.ObjectType(
+                    th.Property("id", th.StringType),
+                    th.Property("name", th.StringType),
                 ),
-                th.Property(
-                    "listBefore",
-                    th.ObjectType(
-                        th.Property("id", th.StringType),
-                        th.Property("name", th.StringType),
-                    ),
+            ),
+            th.Property(
+                "listBefore",
+                th.ObjectType(
+                    th.Property("id", th.StringType),
+                    th.Property("name", th.StringType),
                 ),
-                th.Property(
-                    "listAfter",
-                    th.ObjectType(
-                        th.Property("id", th.StringType),
-                        th.Property("name", th.StringType),
-                    ),
+            ),
+            th.Property(
+                "listAfter",
+                th.ObjectType(
+                    th.Property("id", th.StringType),
+                    th.Property("name", th.StringType),
                 ),
-                th.Property(
-                    "checklist",
-                    th.ObjectType(
-                        th.Property("id", th.StringType),
-                        th.Property("name", th.StringType),
-                    ),
+            ),
+            th.Property(
+                "checklist",
+                th.ObjectType(
+                    th.Property("id", th.StringType),
+                    th.Property("name", th.StringType),
                 ),
-                th.Property(
-                    "checkItem",
-                    th.ObjectType(
-                        th.Property("id", th.StringType),
-                        th.Property("name", th.StringType),
-                        th.Property("state", th.StringType),
-                    ),
+            ),
+            th.Property(
+                "checkItem",
+                th.ObjectType(
+                    th.Property("id", th.StringType),
+                    th.Property("name", th.StringType),
+                    th.Property("state", th.StringType),
                 ),
             ),
         ),
-        th.Property("appCreator", th.BooleanType),
-        th.Property("type", th.StringType),
-        th.Property("date", th.StringType),
-        th.Property("limits", th.ObjectType()),
-        th.Property(
-            "memberCreator",
-            th.ObjectType(
-                th.Property("id", th.StringType),
-                th.Property("activityBlocked", th.BooleanType),
-                th.Property("fullName", th.StringType),
-                th.Property("initials", th.StringType),
-                th.Property("username", th.StringType),
-            ),
+    ),
+    th.Property("appCreator", th.BooleanType),
+    th.Property("type", th.StringType),
+    th.Property("date", th.StringType),
+    th.Property("limits", th.ObjectType()),
+    th.Property(
+        "memberCreator",
+        th.ObjectType(
+            th.Property("id", th.StringType),
+            th.Property("activityBlocked", th.BooleanType),
+            th.Property("fullName", th.StringType),
+            th.Property("initials", th.StringType),
+            th.Property("username", th.StringType),
         ),
-    )
+    ),
+)
--- tap_trello/schemas/cards.py
+++ tap_trello/schemas/cards.py
@@ -2,76 +2,72 @@
 
 from singer_sdk import typing as th
 
-from tap_trello.schemas.utils.custom_object import CustomObject
-
-
-class CardsObject(CustomObject):
-    properties = th.PropertiesList(
-        th.Property("id", th.StringType),
-        th.Property(
-            "badges",
-            th.ObjectType(
-                th.Property(
-                    "attachmentByType",
-                    th.ObjectType(
-                        th.Property(
-                            "trello",
-                            th.ObjectType(
-                                th.Property("board", th.NumberType),
-                                th.Property("card", th.NumberType),
-                            ),
-                        )
-                    ),
-                ),
-                th.Property("location", th.BooleanType),
-                th.Property("votes", th.NumberType),
-                th.Property("viewingMemberVoted", th.BooleanType),
-                th.Property("subscribed", th.BooleanType),
-                th.Property("fogbugz", th.StringType),
-                th.Property("checkItems", th.NumberType),
-                th.Property("checkItemsChecked", th.NumberType),
-                th.Property("checkItemsEarliestDue", th.StringType),
-                th.Property("comments", th.NumberType),
-                th.Property("attachments", th.NumberType),
-                th.Property("description", th.BooleanType),
-                th.Property("due", th.StringType),
-                th.Property("dueComplete", th.BooleanType),
-                th.Property("start", th.BooleanType),
-            ),
-        ),
-        th.Property(
-            "checkItemStates",
-            th.ArrayType(
-                th.PropertiesList(
-                    th.Property("idCheckItem", th.StringType),
-                    th.Property("state", th.StringType),
+CardsObject = th.PropertiesList(
+    th.Property("id", th.StringType),
+    th.Property(
+        "badges",
+        th.ObjectType(
+            th.Property(
+                "attachmentByType",
+                th.ObjectType(
+                    th.Property(
+                        "trello",
+                        th.ObjectType(
+                            th.Property("board", th.NumberType),
+                            th.Property("card", th.NumberType),
+                        ),
+                    )
                 ),
             ),
+            th.Property("location", th.BooleanType),
+            th.Property("votes", th.NumberType),
+            th.Property("viewingMemberVoted", th.BooleanType),
+            th.Property("subscribed", th.BooleanType),
+            th.Property("fogbugz", th.StringType),
+            th.Property("checkItems", th.NumberType),
+            th.Property("checkItemsChecked", th.NumberType),
+            th.Property("checkItemsEarliestDue", th.StringType),
+            th.Property("comments", th.NumberType),
+            th.Property("attachments", th.NumberType),
+            th.Property("description", th.BooleanType),
+            th.Property("due", th.StringType),
+            th.Property("dueComplete", th.BooleanType),
+            th.Property("start", th.BooleanType),
         ),
-        th.Property("closed", th.BooleanType),
-        th.Property("dueComplete", th.BooleanType),
-        th.Property("dateLastActivity", th.StringType),
-        th.Property("desc", th.StringType),
-        th.Property("due", th.StringType),
-        th.Property("dueReminder", th.NumberType),
-        th.Property("email", th.StringType),
-        th.Property("idBoard", th.StringType),
-        th.Property("idChecklists", th.ArrayType(th.StringType)),
-        th.Property("idList", th.StringType),
-        th.Property("idMembers", th.ArrayType(th.StringType)),
-        th.Property("idMembersVoted", th.ArrayType(th.StringType)),
-        th.Property("idShort", th.NumberType),
-        th.Property("idAttachmentCover", th.StringType),
-        th.Property("labels", th.ArrayType(th.ObjectType())),
-        th.Property("idLabels", th.ArrayType(th.StringType)),
-        th.Property("manualCoverAttachment", th.BooleanType),
-        th.Property("name", th.StringType),
-        th.Property("pos", th.NumberType),
-        th.Property("shortLink", th.StringType),
-        th.Property("shortUrl", th.StringType),
-        th.Property("start", th.StringType),
-        th.Property("subscribed", th.BooleanType),
-        th.Property("url", th.StringType),
-        th.Property("isTemplate", th.BooleanType),
-        th.Property("cardRole", th.StringType),
-    )
+    ),
+    th.Property(
+        "checkItemStates",
+        th.ArrayType(
+            th.PropertiesList(
+                th.Property("idCheckItem", th.StringType),
+                th.Property("state", th.StringType),
+            ),
+        ),
+    ),
+    th.Property("closed", th.BooleanType),
+    th.Property("dueComplete", th.BooleanType),
+    th.Property("dateLastActivity", th.StringType),
+    th.Property("desc", th.StringType),
+    th.Property("due", th.StringType),
+    th.Property("dueReminder", th.NumberType),
+    th.Property("email", th.StringType),
+    th.Property("idBoard", th.StringType),
+    th.Property("idChecklists", th.ArrayType(th.StringType)),
+    th.Property("idList", th.StringType),
+    th.Property("idMembers", th.ArrayType(th.StringType)),
+    th.Property("idMembersVoted", th.ArrayType(th.StringType)),
+    th.Property("idShort", th.NumberType),
+    th.Property("idAttachmentCover", th.StringType),
+    th.Property("labels", th.ArrayType(th.ObjectType())),
+    th.Property("idLabels", th.ArrayType(th.StringType)),
+    th.Property("manualCoverAttachment", th.BooleanType),
+    th.Property("name", th.StringType),
+    th.Property("pos", th.NumberType),
+    th.Property("shortLink", th.StringType),
+    th.Property("shortUrl", th.StringType),
+    th.Property("start", th.StringType),
+    th.Property("subscribed", th.BooleanType),
+    th.Property("url", th.StringType),
+    th.Property("isTemplate", th.BooleanType),
+    th.Property("cardRole", th.StringType),
+)
